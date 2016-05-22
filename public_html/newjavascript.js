dd02967f1c03f08a086a165fd279e06b44ccb18d
Dendra <Dendra@10.0.0.4>
2016-05-22 22:44:31 +0200

    First Javascript

---
+++ public_html/newjavascript.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
+
