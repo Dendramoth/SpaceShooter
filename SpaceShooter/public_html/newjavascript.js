753daa17113e88bf9f264ba4129be906a07bb8b5
nepik <nepik@10.0.0.2>
2016-05-22 23:17:53 +0200

    Peta first commit

---
+++ SpaceShooter/public_html/newjavascript.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
+
+function petaFunction(p1, p2) {
+    return p1 * p2;              // The function returns the product of p1 and p2
+}
+
